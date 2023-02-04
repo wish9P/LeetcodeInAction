ac948c08aa64b8c388b79547dd45886c374934e4
wish9P <123935681+wish9P@users.noreply.github.com>
2023-02-05 02:06:23 +0800

    Add leetcode topic 21

---
+++ leetcode-easy/version-c++/21、合并两个有序链表.cpp
@@ -0,0 +1,58 @@
+/*
+  将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+  示例 1：
+    输入：l1 = [1,2,4], l2 = [1,3,4]
+    输出：[1,1,2,3,4,4]
+  
+  示例 2：
+    输入：l1 = [], l2 = []
+    输出：[]
+  
+  示例 3：
+    输入：l1 = [], l2 = [0]
+    输出：[0]
+
+
+  提示：
+    两个链表的节点数目范围是 [0, 50]
+    -100 <= Node.val <= 100
+    l1 和 l2 均按 非递减顺序 排列
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* p = dummy;
+        ListNode* p1 = list1;
+        ListNode* p2 = list2;
+        while (p1 != nullptr && p2 != nullptr) {
+            if (p1->val > p2->val) {
+                p->next = p2;
+                p2 = p2->next;
+            } else {
+                p->next = p1;
+                p1 = p1->next;
+            }
+            p = p->next;
+        }
+        if (p1 != nullptr) {
+            p->next = p1;
+        }
+        if (p2 != nullptr) {
+            p->next = p2;
+        }
+        return dummy->next;
+    }
+};
